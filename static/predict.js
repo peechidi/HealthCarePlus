15a0b05a6f0864e852321b177027a12b5666a448
peechidi <69008790+peechidi@users.noreply.github.com>
2024-10-29 22:55:16 +0100

    Update Prediction

--- static/predict.js
+++ static/predict.js
@@ -12,7 +12,7 @@ async function getDiagnosis(event) {
         muscle_pain: document.getElementById('muscle_pain').checked ? 1 : 0
     };
 
-    const response = await fetch('http://127.0.0.1:5000/predict', {
+    const response = await fetch('/predict', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(symptoms)
